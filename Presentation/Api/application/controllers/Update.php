a91d3358b401aad49aba79d787a9a8a590e74401
xinqiyang <xinqiyang@gmail.com>
2012-06-04 04:38:30 +0800

    add update client interface

---
+++ Presentation/Api/application/controllers/Update.php
@@ -0,0 +1,19 @@
+<?php
+
+class UpdateController extends Yaf_Controller_abstract {
+	
+	protected $_code = Common_Errorcode::SUCCESS;
+	protected $_data = '';
+	
+	public function androidAction() {
+		$this->_data = C('version.android');
+		jsonReturn($this->_code,$this->_data);
+	}
+	
+	public function iosAction()
+	{
+		$this->_data = C('version.ios');;
+		jsonReturn($this->_code,$this->_data);
+	}
+	
+}
